96f41c6c27e7d2bdb6fcb91576ecf8a982b4ed15
satyam <mishrasatyamsam@gmail.com>
2024-11-09 10:51:52 +0530

    Basic Body structure done and routing setup done with Header and Footer

--- README.md
+++ README.md
@@ -13,3 +13,17 @@
 
 - install daisyui library
   `https://daisyui.com/docs/install/`
+
+- NavBar component where use navbar from daisyui is used
+- Install react-router-dom and use it in App.jsx
+- Create BrowserRouter -> Routes ->Route -> Body -> Multiple routes as children
+- Create Outlet in Body Component
+
+` Router Structure`
+
+- Body
+  - NavBar
+  - Route= / => /feed
+  - Route= /login => /login
+  - Route= /profile => /profile
+  - Route= /connections => /connections
--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavBar from "./NavBar";
+import Body from "./Body";
+import Profile from "./Profile";
+import Login from "./Login";
 
 function App() {
   return (
     <>
-      <NavBar />  
+      <BrowserRouter basename="/">
+        <Routes>
+          <Route path="/" element={<Body />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
